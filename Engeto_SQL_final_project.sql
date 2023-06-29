e2d86c73205eb0a4e2d0668f35c19201e5f89b9a
Damian Ebner <damian.ebner@live.com>
2023-06-29 19:55:14 +0300

    Update Engeto_SQL_final_project.sql

--- Engeto_SQL_final_project.sql
+++ Engeto_SQL_final_project.sql
@@ -1,6 +1,3 @@
-
-
-
 CREATE OR REPLACE TABLE czechia_payroll_new AS
 	SELECT
 		AVG(czpay.value) AS salary,
@@ -14,7 +11,6 @@ CREATE OR REPLACE TABLE czechia_payroll_new AS
 	GROUP BY czpay.industry_branch_code, czpay.payroll_year
 	ORDER BY czpay.payroll_year, czpay.industry_branch_code;
 
-
 SELECT 
 	AVG(czechia_price.value) AS value, 
 	czechia_price.category_code,
@@ -23,27 +19,20 @@ FROM czechia_price
 WHERE czechia_price .region_code IS NULL
 GROUP BY czechia_price.category_code, YEAR(czechia_price.date_from);
 
-
-
-
-
-
-
 SELECT *
 FROM czechia_payroll_new;
 
-
 CREATE OR REPLACE TABLE czechia_prices_new AS
-	SELECT 
+SELECT 
 		AVG(czpri.value) AS value, 
 		czpri.category_code,
 		czpc.name AS item,
 		YEAR(czpri.date_from) AS year_price 
-	FROM czechia_price As czpri
-	JOIN czechia_price_category czpc 
-		ON czpri.category_code = czpc.code 
-	WHERE czpri.region_code IS NULL
-	GROUP BY czpri.category_code, YEAR(czpri.date_from);
+FROM czechia_price As czpri
+JOIN czechia_price_category czpc 
+ON czpri.category_code = czpc.code 
+WHERE czpri.region_code IS NULL
+GROUP BY czpri.category_code, YEAR(czpri.date_from);
 
 SELECT *
 FROM czechia_prices_new;
@@ -53,7 +42,7 @@ CREATE OR REPLACE TABLE t_damian_ebner_project_SQL_primary_final AS
 	SELECT *
 	FROM czechia_payroll_new czpn 
 	JOIN czechia_prices_new czpn2 
-		ON czpn.year_salary = czpn2.year_price;
+	ON czpn.year_salary = czpn2.year_price;
 
 
 SELECT *
@@ -61,30 +50,28 @@ FROM t_damian_ebner_project_sql_primary_final;
 
 
 CREATE OR REPLACE TABLE t_damian_ebner_project_SQL_secondary_final AS
-	SELECT 
+SELECT 
 		e.country,
 		e.`year`,
 		e.GDP,
 		e.population,
 		e.gini
-	FROM economies e 
-	JOIN countries c 
-		ON e.country = c.country 
-	WHERE c.continent = 'Europe'
-		AND e.`year` BETWEEN 2006 AND 2018
-	ORDER BY e.country, e.`year`;
+FROM economies e 
+JOIN countries c 
+ON e.country = c.country 
+WHERE c.continent = 'Europe'
+AND e.`year` BETWEEN 2006 AND 2018
+ORDER BY e.country, e.`year`;
 
-
-
-
+-- Question FIRST
 WITH salaries AS (
-	SELECT DISTINCT 
+SELECT DISTINCT 
 		year_salary,
 		industry_branch_code,
 		industry,
 		salary
-	FROM t_damian_ebner_project_SQL_primary_final AS dep  
-	WHERE industry_branch_code IS NOT NULL
+FROM t_damian_ebner_project_SQL_primary_final AS dep  
+WHERE industry_branch_code IS NOT NULL
 	)
 SELECT 
 	*,
@@ -93,6 +80,8 @@ SELECT
 FROM salaries
 ORDER BY salary_growth;
 
+
+-- Question SECOND 
 SELECT 
 	salary,
 	industry,
@@ -105,17 +94,16 @@ WHERE year_salary IN (2006, 2018)
 	AND (item LIKE '%mléko%' OR item LIKE '%chléb%')
 	AND industry_branch_code IS NULL;
 
-
-
+-- Question THIRD
 SELECT 
 	y.*,
 	ROUND((y.value - y.previous_price) * 100 / y.previous_price, 2) AS price_result
 FROM (
-	SELECT 
+SELECT 
 		x.*,
 		ROUND(LAG(value) OVER (PARTITION BY category_code ORDER BY year_price), 3) AS previous_price
 FROM (
-		SELECT DISTINCT 
+SELECT DISTINCT 
 			year_price,
 			value,
 			category_code,
@@ -126,18 +114,20 @@ FROM t_damian_ebner_project_sql_primary_final
 WHERE y.previous_price IS NOT NULL
 ORDER BY price_result;
 
+
+-- Question FOURTH
 SELECT
 	*,
 	LAG(salary) OVER (ORDER BY year_salary) AS previous_salary
 FROM (
-	SELECT DISTINCT 
+SELECT DISTINCT 
 		year_salary,
 		salary
 FROM t_damian_ebner_project_sql_primary_final 
-	WHERE industry_branch_code IS NULL
+WHERE industry_branch_code IS NULL
 	) x;
 
-
+-- Question FIFTH
 SELECT 
 	`year`,
 	GDP,
