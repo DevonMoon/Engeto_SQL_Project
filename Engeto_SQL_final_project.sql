a4af1126ceb2e17e29fb262255d2268065021f4d
Damian Ebner <damian.ebner@live.com>
2023-06-29 19:58:27 +0300

    Update Engeto_SQL_final_project.sql

--- Engeto_SQL_final_project.sql
+++ Engeto_SQL_final_project.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE TABLE czechia_payroll_new AS
-	SELECT
+SELECT
 		AVG(czpay.value) AS salary,
 		czpay.industry_branch_code,
 		czpib.name AS industry,
 		czpay.payroll_year AS year_salary
-	FROM czechia_payroll czpay 
-	LEFT JOIN czechia_payroll_industry_branch czpib  
-		ON czpay.industry_branch_code = czpib.code 
-	WHERE czpay.value_type_code = 5958
-	GROUP BY czpay.industry_branch_code, czpay.payroll_year
-	ORDER BY czpay.payroll_year, czpay.industry_branch_code;
+FROM czechia_payroll czpay 
+LEFT JOIN czechia_payroll_industry_branch czpib  
+	 ON czpay.industry_branch_code = czpib.code 
+WHERE czpay.value_type_code = 5958
+GROUP BY czpay.industry_branch_code, czpay.payroll_year
+ORDER BY czpay.payroll_year, czpay.industry_branch_code;
 
 SELECT 
 	AVG(czechia_price.value) AS value, 
